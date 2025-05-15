403face109d7c299b664743af5bc38eabf162337
Artemis Li <auroraziling@gmail.com>
2025-05-15 11:33:53 +0800

    fix: `MainWindow` content attaches to visual tree multiple times

--- src/Everywhere/Views/Windows/MainWindow.axaml.cs
+++ src/Everywhere/Views/Windows/MainWindow.axaml.cs
@@ -8,4 +8,13 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        // MainWindow would be easily closed and reopened.
+        // Its content should be null before closing to make it detach from visual tree.
+        // Otherwise, it will try to attach to the visual tree again (Exception).
+        Content = null;
+        base.OnClosed(e);
+    }
 }
