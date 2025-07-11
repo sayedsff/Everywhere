a22f73fa9d37190374f31c41fac4dcb11bafdaf6
Dear.Va <dear.va.love@gmail.com>
2025-07-11 22:24:55 +0800

    feat: Markdown Renderer
    Special thanks to @AuroraZiling

---
+++ src/Everywhere.Markdown/MarkdownRenderer.Utils.cs
@@ -0,0 +1,134 @@
+﻿// @author https://github.com/DearVa
+
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Avalonia.Controls;
+using Avalonia.Controls.Documents;
+using Markdig.Syntax;
+
+namespace Everywhere.Markdown;
+
+public partial class MarkdownRenderer
+{
+    /// <summary>
+    /// Utility class to map <see cref="Control"/> to <see cref="BlockNode"/> itself
+    /// </summary>
+    /// <param name="target"></param>
+    private class InlinesProxy(InlineCollection target)
+    {
+        public int Count => children.Count;
+
+        public InlineNode this[int index]
+        {
+            get => children[index];
+            set
+            {
+                target[index] = value.Inline;
+                children[index] = value;
+            }
+        }
+
+        private readonly List<InlineNode> children = [];
+
+        public void Add(InlineNode node)
+        {
+            children.Add(node);
+            target.Add(node.Inline);
+            Debug.Assert(children.Count == target.Count, "Children count mismatch");
+        }
+
+        public void RemoveAt(int index)
+        {
+            var node = children[index];
+            target.Remove(node.Inline);
+            children.RemoveAt(index);
+            Debug.Assert(children.Count == target.Count, "Children count mismatch");
+        }
+    }
+
+    /// <summary>
+    /// Utility class to map <see cref="Control"/> to <see cref="BlockNode"/> itself
+    /// </summary>
+    /// <param name="target"></param>
+    private class BlocksProxy(Controls target)
+    {
+        private class MockBlockNode(Control control) : BlockNode
+        {
+            private const string NotSupportedMessage = "This node is a mock and does not support this operation.";
+
+            public override Control Control => control;
+
+            private Control control = control;
+
+            public void SetControl(Control control) => this.control = control;
+
+            protected override bool IsCompatible(MarkdownObject markdownObject)
+            {
+                throw new NotSupportedException(NotSupportedMessage);
+            }
+
+            protected override bool UpdateCore(
+                MarkdownObject markdownObject,
+                in ObservableStringBuilderChangedEventArgs change,
+                CancellationToken cancellationToken)
+            {
+                throw new NotSupportedException(NotSupportedMessage);
+            }
+        }
+
+        public int Count => children.Count;
+
+        public BlockNode this[int index]
+        {
+            get => children[index];
+            set
+            {
+                target[index] = value.Control;
+                children[index] = value;
+            }
+        }
+
+        private readonly List<BlockNode> children = [];
+
+        public void Add(BlockNode node)
+        {
+            children.Add(node);
+            target.Add(node.Control);
+            Debug.Assert(children.Count == target.Count, "Children count mismatch");
+        }
+
+        /// <summary>
+        /// Adds a control to the target collection for special cases where a mock node is needed.
+        /// This should be treated carefully as it adds a mock node.
+        /// </summary>
+        /// <param name="control"></param>
+        public void Add(Control control)
+        {
+            children.Add(new MockBlockNode(control));
+            target.Add(control);
+        }
+
+        public void SetControlAt(int index, Control control)
+        {
+            target[index] = control;
+            if (children[index] is MockBlockNode mockBlockNode) mockBlockNode.SetControl(control);
+            else children[index] = new MockBlockNode(control);
+        }
+
+        public void RemoveAt(int index)
+        {
+            var node = children[index];
+            target.Remove(node.Control);
+            children.RemoveAt(index);
+            Debug.Assert(children.Count == target.Count, "Children count mismatch");
+        }
+    }
+
+    [Conditional("DEBUG")]
+    private static void PrintMetrics(string metrics, [CallerMemberName] string memberName = "")
+    {
+        var message = $"[{DateTime.Now:G}] ({memberName}) {metrics}";
+        if (Debugger.IsAttached) Debug.WriteLine(message);
+        else Console.WriteLine(message);
+    }
+}
---
+++ src/Everywhere.Markdown/MarkdownRenderer.axaml.cs
@@ -0,0 +1,110 @@
+﻿// @author https://github.com/DearVa
+// @author https://github.com/AuroraZiling
+// @author https://github.com/SlimeNull
+
+using System.Diagnostics;
+using System.Threading.Channels;
+using Avalonia;
+using Avalonia.Controls;
+using Markdig;
+
+namespace Everywhere.Markdown;
+
+public partial class MarkdownRenderer : Control
+{
+    public ObservableStringBuilder MarkdownBuilder { get; } = new();
+
+    private Channel<ObservableStringBuilderChangedEventArgs>? renderChannel;
+
+    private readonly DocumentNode documentNode = new();
+    private readonly MarkdownPipeline pipeline = new MarkdownPipelineBuilder()
+        .UseAdvancedExtensions()
+        .UseCodeBlockSpanFixer()
+        .Build();
+
+    public MarkdownRenderer()
+    {
+        LogicalChildren.Add(documentNode.Control);
+        VisualChildren.Add(documentNode.Control);
+        MarkdownBuilder.Changed += HandleMarkdownBuilderChanged;
+    }
+
+    private async void RenderProcessAsync(ChannelReader<ObservableStringBuilderChangedEventArgs> reader)
+    {
+        try
+        {
+            while (await reader.WaitToReadAsync())
+            {
+                while (reader.TryRead(out var e))
+                {
+                    var markdown = e.NewString;
+                    var timer = Stopwatch.StartNew();
+                    var document = await Task.Run(() => Markdig.Markdown.Parse(markdown, pipeline));
+                    PrintMetrics($"Parse markdown in {timer.Elapsed.TotalMicroseconds} micro sec.");
+                    timer.Restart();
+                    documentNode.Update(document, e, CancellationToken.None);
+                    PrintMetrics($"Render markdown in {timer.Elapsed.TotalMicroseconds} micro sec.");
+                }
+            }
+        }
+        catch (OperationCanceledException) { }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error rendering markdown\n{ex}");
+        }
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property != IsVisibleProperty) return;
+        if (change.NewValue is true)
+        {
+            BeginRender();
+        }
+        else
+        {
+            EndRender();
+        }
+    }
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+        if (IsVisible)
+        {
+            BeginRender();
+        }
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnDetachedFromVisualTree(e);
+        EndRender();
+    }
+
+    private void BeginRender()
+    {
+        if (renderChannel is not null) return;
+        renderChannel = Channel.CreateUnbounded<ObservableStringBuilderChangedEventArgs>();
+        RenderProcessAsync(renderChannel);
+        renderChannel.Writer.TryWrite(
+            new ObservableStringBuilderChangedEventArgs(
+                MarkdownBuilder.ToString(),
+                0,
+                MarkdownBuilder.Length));
+    }
+
+    private void EndRender()
+    {
+        if (renderChannel is null) return;
+        renderChannel.Writer.Complete();
+        renderChannel = null;
+    }
+
+    private void HandleMarkdownBuilderChanged(in ObservableStringBuilderChangedEventArgs e)
+    {
+        renderChannel?.Writer.WriteAsync(e).AsTask().Wait();
+    }
+}
