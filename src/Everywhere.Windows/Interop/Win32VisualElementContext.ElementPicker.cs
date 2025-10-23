f8bebd1bd1782fc995c7d7e4e210278b41dc03be
Dear.Va <dear.va.love@gmail.com>
2025-10-23 21:24:30 +0800

    fix: element picker cannot be closed by mouse right click

--- src/Everywhere.Windows/Interop/Win32VisualElementContext.ElementPicker.cs
+++ src/Everywhere.Windows/Interop/Win32VisualElementContext.ElementPicker.cs
@@ -16,7 +16,10 @@ namespace Everywhere.Windows.Interop;
 
 public partial class Win32VisualElementContext
 {
-    private class ElementPicker : Window
+    /// <summary>
+    /// A window that allows the user to pick an element from the screen.
+    /// </summary>
+    private class ElementPickerWindow : Window
     {
         private readonly Win32VisualElementContext _context;
         private readonly IWindowHelper _windowHelper;
@@ -32,7 +35,7 @@ public partial class Win32VisualElementContext
         private Rect? _previousMaskRect;
         private IVisualElement? _selectedElement;
 
-        private ElementPicker(
+        private ElementPickerWindow(
             Win32VisualElementContext context,
             IWindowHelper windowHelper,
             PickElementMode mode)
@@ -88,8 +91,12 @@ public partial class Win32VisualElementContext
 
         protected override unsafe void OnPointerEntered(PointerEventArgs e)
         {
+            // Simulate a mouse left button down in the top-left corner of the window (8,8 to avoid the border)
+            var x = (_screenBounds.X + 8d) / _screenBounds.Width * 65535;
+            var y = (_screenBounds.Y + 8d) / _screenBounds.Height * 65535;
+
             // SendInput MouseLeftButtonDown, this will:
-            // 1. prevent the cursor from changing to the default arrow cursor and interacting with other windows
+            // 1. prevent the cursor from changing to the default arrow cursor and interacting with other windows (behaviors like Spy++ etc.)
             // 2. Trigger the OnPointerPressed event to set the window to hit test invisible
             PInvoke.SendInput(
                 new ReadOnlySpan<INPUT>(
@@ -102,8 +109,8 @@ public partial class Win32VisualElementContext
                             mi = new MOUSEINPUT
                             {
                                 dwFlags = MOUSE_EVENT_FLAGS.MOUSEEVENTF_LEFTDOWN | MOUSE_EVENT_FLAGS.MOUSEEVENTF_ABSOLUTE,
-                                dx = Position.X + 1,
-                                dy = Position.Y + 1
+                                dx = (int)x,
+                                dy = (int)y
                             }
                         }
                     },
@@ -111,10 +118,30 @@ public partial class Win32VisualElementContext
                 sizeof(INPUT));
         }
 
+        private bool _isLeftButtonPressed;
+        private LowLevelMouseHook? _mouseHook;
+
         protected override void OnPointerPressed(PointerPressedEventArgs e)
         {
+            // This should be triggered by the SendInput above
+            if (_isLeftButtonPressed || !e.Properties.IsLeftButtonPressed) return;
+
+            _isLeftButtonPressed = true;
             _windowHelper.SetHitTestVisible(this, false);
 
+            // Install a low-level mouse hook to listen for right button down events
+            // This is needed because once we set the window to hit test invisible
+            _mouseHook ??= new LowLevelMouseHook((param, ref _, ref _) =>
+            {
+                // Close the window and cancel selection on right button down
+                if (param == (nuint)WINDOW_MESSAGE.WM_RBUTTONDOWN)
+                {
+                    _selectedElement = null;
+                    Close();
+                }
+            });
+
+            // Pick the element under the cursor immediately
             if (PInvoke.GetCursorPos(out var point)) Pick(point);
         }
 
@@ -125,11 +152,6 @@ public partial class Win32VisualElementContext
 
         protected override void OnPointerReleased(PointerReleasedEventArgs e)
         {
-            if (e.InitialPressMouseButton != MouseButton.Left)
-            {
-                _selectedElement = null;
-            }
-
             Close();
         }
 
@@ -142,6 +164,7 @@ public partial class Win32VisualElementContext
 
         protected override void OnClosed(EventArgs e)
         {
+            _mouseHook?.Dispose();
             _bitmap.Dispose();
             _taskCompletionSource.TrySetResult(_selectedElement);
         }
@@ -181,6 +204,7 @@ public partial class Win32VisualElementContext
                 }
                 case PickElementMode.Element:
                 {
+                    // TODO: sometimes this only picks the window, not the element under the cursor?
                     _selectedElement = _context.TryFrom(() => Automation.FromPoint(point));
                     if (_selectedElement == null) break;
 
@@ -209,7 +233,7 @@ public partial class Win32VisualElementContext
             IWindowHelper windowHelper,
             PickElementMode mode)
         {
-            var window = new ElementPicker(context, windowHelper, mode);
+            var window = new ElementPickerWindow(context, windowHelper, mode);
             window.Show();
             return window._taskCompletionSource.Task;
         }
